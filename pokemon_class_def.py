d6ea54a7838bff2c8eabbbef4586673b0b6b59cb
max.breitmeyer <max.breitmeyer@gmail.com>
2023-05-04 15:43:14 -0400

    added more elements to the pokemon class

--- pokemon_class_def.py
+++ pokemon_class_def.py
@@ -1,6 +1,8 @@
 
 class Pokemon:
-    def __init__(self, ability, move_list, name):
+    def __init__(self, ability, move_list, name, type, stats):
         self.ability = ability
         self.move_list = move_list
         self.name = name
+        self.type = type
+        self.stats = stats
