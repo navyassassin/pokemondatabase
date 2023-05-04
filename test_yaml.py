5aa22ec2791486022ff84f0e487dd9a6dbecf4c1
max.breitmeyer <max.breitmeyer@gmail.com>
2023-05-04 15:40:35 -0400

    loads yamls

--- test_yaml.py
+++ test_yaml.py
@@ -1,10 +1,32 @@
 import yaml
 
+abilities = "./abilities.yaml"
+moves = "./moves.yaml"
+pokedex = "./pokedex.yaml"
+stats = "./stats.yaml"
+types = "./type_table.yaml"
+
+
 def main():
-    with open("./pokemondatabase/type_table.yaml", 'r') as testFile:
-        pokedexYaml = yaml.load(testFile, Loader=yaml.Loader)
-    print(type(pokedexYaml))
-    print(pokedexYaml)
+    with open(abilities, 'r') as f:
+        abilitiesDict = yaml.load(f, Loader=yaml.Loader)
+    print(abilitiesDict)
+    
+    with open(moves, 'r') as f:
+        movesDict = yaml.load(f, Loader=yaml.Loader)
+    print(movesDict)
+    
+    with open(pokedex, 'r') as f:
+        pokedexDict = yaml.load(f, Loader=yaml.Loader)
+    print(pokedexDict)
+    
+    with open(stats, 'r') as f:
+        statsDict = yaml.load(f, Loader=yaml.Loader)
+    print(statsDict)
+
+    with open(types, 'r') as f:
+        typesDict = yaml.load(f, Loader=yaml.Loader)
+    print(typesDict)
 
 if __name__ == "__main__":
     main()
